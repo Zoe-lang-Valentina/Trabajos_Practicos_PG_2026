b780ef988c3e25c3f04e80736fe04e92a2eedf7a
alumno <alumno@Instituto.local>
2026-06-11 10:18:22 -0300

    Update app.js 11/6

--- TP5/app.js
+++ TP5/app.js
@@ -1,37 +1,6 @@
 //Declarar variable
-let num1 = 67
-let num2 = 1
-
+let pais = "Argentina"
 //Asignar Funcion
- Suma = num1 + num2
- Resta = num1 - num2
- Multiplicacion = num1 * num2
- Division = num1 / num2 
-
-console.log( "El resultado de la multiplicacion es:" + Multiplicacion)
-console.log("El resultado de la division es:" + Division)
- alert("El resultado de la suma es:" + Suma )
- alert( "El resultado de la resta es:" +Resta )
-
-//Declarar Variable
-
-let Nombre_Alumno = "Gaston" 
-let Apellido_Alumno = "Schafe"
-let Edad_Alumno = 67
-
-console.log("El Alumno: " + Nombre_Alumno + " " + Apellido_Alumno + " " + Edad_Alumno)
-
-let precio1 = 89
-let precio2 = 40
-let precio3 = 23
-Total = precio1 + precio2 + precio3
-console.log( Total )
-
-let Comida_favorita = "los canelones"
-let Color_favorito = "verde"
-let Numeros_favorito = 23
-
-console.log("Mi comida favorita es" + Comida_favorita + "me gusta el color"+colorfavorito+"y mi número favorito es" + numerofavorito)
-
-let Pais= "Argentina"
-console.log(Pais)
+ console.log( pais)
+pais = "Brasil"
+console.log(pais)
