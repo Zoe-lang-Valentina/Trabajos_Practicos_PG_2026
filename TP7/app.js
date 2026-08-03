446e55e5998dc68779522135092cf5ba73a14302
alumno <alumno@Instituto.local>
2026-08-03 08:19:39 -0300

    3/8

---
+++ TP7/app.js
@@ -0,0 +1,15 @@
+let ej1 = document.querySelector
+let b1 = document.querySelector
+let edad = 18
+
+b1.onclick = function () {
+    if (edad >= 18) {
+        ej1.style.TextContent = 'ere mayor de edad'
+    }else{
+        ej1.style.textcontent= 'tu no ere mayor de edad '
+    }
+}
+//:v
+
+let ej2 = document.querySelector
+let b2 = document.
