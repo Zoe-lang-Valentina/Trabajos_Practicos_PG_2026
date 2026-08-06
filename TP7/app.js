4c51109323456f59ada1e6191fa95a6e45249cb3
alumno <alumno@Instituto.local>
2026-08-06 10:40:45 -0300

    6/2026

--- TP7/app.js
+++ TP7/app.js
@@ -1,15 +1,57 @@
-let ej1 = document.querySelector
-let b1 = document.querySelector
+let ej1 = document.querySelector('#ej1')
+let b1 = document.querySelector('#b1')
 let edad = 18
 
 b1.onclick = function () {
     if (edad >= 18) {
-        ej1.style.TextContent = 'ere mayor de edad'
+        ej1.textContent = 'ere mayor de edad ψ(._. )>'
     }else{
-        ej1.style.textcontent= 'tu no ere mayor de edad '
+        ej1.textContent= 'tu no ere mayor de edad '
     }
 }
-//:v
+//         :v
 
-let ej2 = document.querySelector
-let b2 = document.
+let ej2 = document.querySelector('#ej2')
+let b2= document.querySelector('#b2')
+let nombreUsuario = 'Pablizqui'
+
+b2.onclick = function () {
+    if ((nombreUsuario == 'Adrian') || (nombreUsuario == 'Pablizqui')){
+        ej2.textContent ='Bienvenido '+ nombreUsuario + '¿como estas?'
+       }
+       else{
+           ej2.textContent = 'bienvenido' + nombreUsuario
+       }
+} 
+
+
+//         :v
+
+let ej4 = document.querySelector('#ej4')
+let b4= document.querySelector('#b4')
+let numero = 0
+
+b4.onclick = function () {
+    if (numero < 0 ){
+        ej4.textContent = 'El numero es negativo'
+    }
+     if( numero > 0) {
+        ej4.textContent = 'El numero es positivo'
+    }
+    if (numero == 0){
+        ej4.textContent = 'El numero es 0'
+    }
+}
+
+//        ;3
+
+let ej5 = document.querySelector('#ej5')
+let b5= document.querySelector('#b5')
+let Edad = 40
+
+if ((Edad > 6) && (Edad < 10)){
+    ej5.textContent= 'Usted esun niño'
+}
+else if ((Edad > 6) && (Edad < 10)){
+   ej5.textContent
+}
